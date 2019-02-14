9c9342f6d66361be5f8bae978738f803e6c3c5f6
github username <github email address>
2019-02-14 09:36:12 -0500

    gh page update

---
+++ build/precache-manifest.babc25259753ab88495277cfd52a5c0e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "a14b348258e9d4859d9afa8e2eca989f",
+    "url": "/brian-yu/static/media/left.a14b3482.svg"
+  },
+  {
+    "revision": "d1040773dfda8b80e522",
+    "url": "/brian-yu/static/css/main.9a83755f.chunk.css"
+  },
+  {
+    "revision": "ad3c35b39e6e65a006ec16dc38de5384",
+    "url": "/brian-yu/static/media/projecttitle.ad3c35b3.svg"
+  },
+  {
+    "revision": "0e83f090aba0b6f990a2",
+    "url": "/brian-yu/static/js/1.0e83f090.chunk.js"
+  },
+  {
+    "revision": "5b00609461df64ba5467",
+    "url": "/brian-yu/static/js/runtime~main.5b006094.js"
+  },
+  {
+    "revision": "9f6cf388a22b1de4f071cc6959addb32",
+    "url": "/brian-yu/static/media/project.9f6cf388.svg"
+  },
+  {
+    "revision": "a85ea8b69893ce419c3df45e6d95bf1d",
+    "url": "/brian-yu/static/media/profile.a85ea8b6.svg"
+  },
+  {
+    "revision": "481053a1260b954d3c069c09a24c881e",
+    "url": "/brian-yu/static/media/skills.481053a1.svg"
+  },
+  {
+    "revision": "d1040773dfda8b80e522",
+    "url": "/brian-yu/static/js/main.d1040773.chunk.js"
+  },
+  {
+    "revision": "95c276cfbb0d34ba8dee43477511769b",
+    "url": "/brian-yu/static/media/right.95c276cf.svg"
+  },
+  {
+    "revision": "345a9396b214eb816f94c5b851dca085",
+    "url": "/brian-yu/static/media/technology.345a9396.svg"
+  },
+  {
+    "revision": "4874ce925f27e45b1da394bc2f5f6df0",
+    "url": "/brian-yu/static/media/ALL.4874ce92.svg"
+  },
+  {
+    "revision": "97c5951c46c6919dd8233acd278b5825",
+    "url": "/brian-yu/static/media/rectangle.97c5951c.svg"
+  },
+  {
+    "revision": "9eb3b851b47a955e0b95e394d5a013d7",
+    "url": "/brian-yu/static/media/hi.9eb3b851.svg"
+  },
+  {
+    "revision": "eed46a7dd9817262583cae39212e6530",
+    "url": "/brian-yu/static/media/ball2.eed46a7d.svg"
+  },
+  {
+    "revision": "0e83f090aba0b6f990a2",
+    "url": "/brian-yu/static/css/1.767a2d94.chunk.css"
+  },
+  {
+    "revision": "13fd8964b5e5c40ba23372548e26c855",
+    "url": "/brian-yu/index.html"
+  }
+];
