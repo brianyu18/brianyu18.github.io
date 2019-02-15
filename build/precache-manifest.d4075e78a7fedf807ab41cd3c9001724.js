d4f287942c7fd143f5523fe6d6d64df1560aba38
brianyu18 <brianyu1990@gmail.com>
2019-02-14 19:49:33 -0500

    testing if we get this stuff working

---
+++ build/precache-manifest.d4075e78a7fedf807ab41cd3c9001724.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "a14b348258e9d4859d9afa8e2eca989f",
+    "url": "./static/media/left.a14b3482.svg"
+  },
+  {
+    "revision": "22f0c87fce3fd97be019",
+    "url": "./static/css/main.2487dd5b.chunk.css"
+  },
+  {
+    "revision": "ad3c35b39e6e65a006ec16dc38de5384",
+    "url": "./static/media/projecttitle.ad3c35b3.svg"
+  },
+  {
+    "revision": "54653843a48ec2691933",
+    "url": "./static/js/1.54653843.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9f6cf388a22b1de4f071cc6959addb32",
+    "url": "./static/media/project.9f6cf388.svg"
+  },
+  {
+    "revision": "a85ea8b69893ce419c3df45e6d95bf1d",
+    "url": "./static/media/profile.a85ea8b6.svg"
+  },
+  {
+    "revision": "481053a1260b954d3c069c09a24c881e",
+    "url": "./static/media/skills.481053a1.svg"
+  },
+  {
+    "revision": "22f0c87fce3fd97be019",
+    "url": "./static/js/main.22f0c87f.chunk.js"
+  },
+  {
+    "revision": "95c276cfbb0d34ba8dee43477511769b",
+    "url": "./static/media/right.95c276cf.svg"
+  },
+  {
+    "revision": "345a9396b214eb816f94c5b851dca085",
+    "url": "./static/media/technology.345a9396.svg"
+  },
+  {
+    "revision": "4874ce925f27e45b1da394bc2f5f6df0",
+    "url": "./static/media/ALL.4874ce92.svg"
+  },
+  {
+    "revision": "97c5951c46c6919dd8233acd278b5825",
+    "url": "./static/media/rectangle.97c5951c.svg"
+  },
+  {
+    "revision": "9eb3b851b47a955e0b95e394d5a013d7",
+    "url": "./static/media/hi.9eb3b851.svg"
+  },
+  {
+    "revision": "eed46a7dd9817262583cae39212e6530",
+    "url": "./static/media/ball2.eed46a7d.svg"
+  },
+  {
+    "revision": "54653843a48ec2691933",
+    "url": "./static/css/1.38c2f712.chunk.css"
+  },
+  {
+    "revision": "afd604501981740db8ec8d479dd06005",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/brian-yu/precache-manifest.b77a52099dc8bbdf04eeef85976ba2d2.js"
+  "./precache-manifest.d4075e78a7fedf807ab41cd3c9001724.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/brian-yu/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
